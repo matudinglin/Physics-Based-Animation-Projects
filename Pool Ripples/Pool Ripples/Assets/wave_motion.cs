7cbd4a6cef0cf6d9c28d66b58f9212e1b1cc4551
matudinglin <45813154+matudinglin@users.noreply.github.com>
2024-01-17 16:07:32 -0800

    One way coupling without optimization

--- Pool Ripples/Pool Ripples/Assets/wave_motion.cs
+++ Pool Ripples/Pool Ripples/Assets/wave_motion.cs
@@ -21,6 +21,10 @@ public class wave_motion : MonoBehaviour
 	Vector3 	cube_v = Vector3.zero;
 	Vector3 	cube_w = Vector3.zero;
 
+	GameObject cube;
+	GameObject block;
+
+
 
 	// Use this for initialization
 	void Start () 
@@ -72,6 +76,9 @@ public class wave_motion : MonoBehaviour
 			old_h[i,j]=0;
 			vh[i,j]=0;
 		}
+
+		cube = GameObject.Find("Cube");
+		block = GameObject.Find("Block");
 	}
 
 	void A_Times(bool[,] mask, float[,] x, float[,] Ax, int li, int ui, int lj, int uj)
@@ -160,14 +167,83 @@ public class wave_motion : MonoBehaviour
 		//TODO: for block 1, calculate low_h.
 		//TODO: then set up b and cg_mask for conjugate gradient.
 		//TODO: Solve the Poisson equation to obtain vh (virtual height).
+		Vector3 cube_pos = cube.transform.position;
+		for (int i=0; i<size; i++)
+		for (int j=0; j<size; j++) 
+		{
+			Vector3 pos = new Vector3(i*0.1f-size*0.05f, h[i,j], j*0.1f-size*0.05f);
+			// if pos is inside the block, set the mask to true
+			if(pos.x<=cube_pos.x+0.5f && pos.x>=cube_pos.x-0.5f && pos.z<=cube_pos.z+0.5f && pos.z>=cube_pos.z-0.5f)
+			{
+				cg_mask[i,j]=true;
+				low_h[i,j]=cube_pos.y - 0.2f;
+				b[i,j]=(new_h[i,j]-low_h[i,j])/rate;
+			}
+			else
+			{
+				cg_mask[i,j]=false;
+				low_h[i,j]=h[i,j];
+				b[i,j]=0;
+				vh[i,j]=0;
+			}
+		}
+		Conjugate_Gradient(cg_mask, b, vh, 0, size-1, 0, size-1);
+		//TODO: Diminish vh.
+		for (int i=0; i<size; i++)
+		for (int j=0; j<size; j++) 
+		{
+			if(cg_mask[i,j]) vh[i,j]*=gamma;
+		}
+		//TODO: Update new_h by vh.
+		for (int i=0; i<size; i++)
+		for (int j=0; j<size; j++) 
+		{
+			if(i-1>=0)	new_h[i,j]+=rate*(vh[i-1,j] - vh[i,j]);
+			if(i+1<size)	new_h[i,j]+=rate*(vh[i+1,j] - vh[i,j]);
+			if(j-1>=0)	new_h[i,j]+=rate*(vh[i,j-1] - vh[i,j]);
+			if(j+1<size)	new_h[i,j]+=rate*(vh[i,j+1] - vh[i,j]);
+		}
 
 		//TODO: for block 2, calculate low_h.
 		//TODO: then set up b and cg_mask for conjugate gradient.
 		//TODO: Solve the Poisson equation to obtain vh (virtual height).
-	
+		Vector3 block_pos = block.transform.position;
+		for (int i=0; i<size; i++)
+		for (int j=0; j<size; j++) 
+		{
+			Vector3 pos = new Vector3(i*0.1f-size*0.05f, h[i,j], j*0.1f-size*0.05f);
+			// if pos is inside the block, set the mask to true
+			if(pos.x<=block_pos.x+0.5f && pos.x>=block_pos.x-0.5f && pos.z<=block_pos.z+0.5f && pos.z>=block_pos.z-0.5f)
+			{
+				cg_mask[i,j]=true;
+				low_h[i,j]=block_pos.y - 0.2f;
+				b[i,j]=(new_h[i,j]-low_h[i,j])/rate;
+			}
+			else
+			{
+				cg_mask[i,j]=false;
+				low_h[i,j]=h[i,j];
+				b[i,j]=0;
+				vh[i,j]=0;
+			}
+		}
+		Conjugate_Gradient(cg_mask, b, vh, 0, size-1, 0, size-1);
 		//TODO: Diminish vh.
+		for (int i=0; i<size; i++)
+		for (int j=0; j<size; j++) 
+		{
+			if(cg_mask[i,j]) vh[i,j]*=gamma;
+		}
 
 		//TODO: Update new_h by vh.
+		for (int i=0; i<size; i++)
+		for (int j=0; j<size; j++) 
+		{
+			if(i-1>=0)	new_h[i,j]+=rate*(vh[i-1,j] - vh[i,j]);
+			if(i+1<size)	new_h[i,j]+=rate*(vh[i+1,j] - vh[i,j]);
+			if(j-1>=0)	new_h[i,j]+=rate*(vh[i,j-1] - vh[i,j]);
+			if(j+1<size)	new_h[i,j]+=rate*(vh[i,j+1] - vh[i,j]);
+		}
 
 		//Step 3
 		//TODO: old_h <- h; h <- new_h;
@@ -190,7 +266,7 @@ public class wave_motion : MonoBehaviour
 		Vector3[] X    = mesh.vertices;
 		float[,] new_h = new float[size, size];
 		float[,] h     = new float[size, size];
-
+		
 		//TODO: Load X.y into h.
 		for (int i=0; i<size; i++)
 		for (int j=0; j<size; j++) 
@@ -201,7 +277,7 @@ public class wave_motion : MonoBehaviour
 		if (Input.GetKeyDown ("r")) 
 		{
 			//TODO: Add random water.
-			float r=Random.Range(0.1f,0.2f);
+			float r=Random.Range(0.1f,0.5f);
 			int x = Random.Range(0,size);
 			int y = Random.Range(0,size);
 			h[x, y]+=r;
